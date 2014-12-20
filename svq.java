dc9ac51fbaee7e0a4668ce8e06420e45efda858c
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2014-12-20 12:23:04 +0100

    Read bmp image

--- svq.java
+++ svq.java
@@ -1,6 +1,25 @@
+// Get JAFFE database from http://www.kasrl.org/jaffe_info.html
+// Extract pics in folder named "jaffe"
+// package image_test;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class svq {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+
+    public static void main(String[] args) {
+        // read image
+        BufferedImage input = null;
+        try {
+            input = ImageIO.read(new File("jaffe/KA.AN1.39.tiff.bmp"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Done!");
+    }
 }
