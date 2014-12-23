d735dce2214b5a223d8f94d55e38f2a6f8df8eac
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2014-12-24 00:29:31 +0100

    Updated VQ code (and more tested)

--- SVQ.java
+++ SVQ.java
@@ -5,75 +5,101 @@
 // Convert to bmp with `ls *.tiff | while read f; do convert "$f" "${f%.*}.bmp"; done`
 
 import java.io.File;
+import java.util.Arrays;
 
 public class SVQ {
 
     Centroid[] centroids;
+    int ncentr, imgsize;
 
-    public SVQ(int ncentr, int centrSize) {
+    public SVQ(int ncentr, int imgsize) {
+        this.ncentr = ncentr;
+        this.imgsize = imgsize;
         centroids = new Centroid[ncentr];
-        for (int i=0; i<centroids.length; i++) {
-            centroids[i] = new Centroid(centrSize);
+        for (int i=0; i<ncentr; i++) {
+            centroids[i] = new Centroid(imgsize);
         }
+    }
+
+    public void checkLengths(double[] a, double[] b){
+        checkLengths(a, b.length);
+    }
+
+    public void checkLengths(double[] a, int bLen){
+        if (a.length != bLen) {
+            throw new RuntimeException(
+                "Vectors lengths don't match.");
+        }
+    }
+
+    public double[] similarities(double[] vec) {
+        checkLengths(vec, imgsize);
+        double[] ret = new double[ncentr];
+        System.out.print("Simils: ");
+        for (int i=0; i<ncentr; i++) {
+            ret[i] = centroids[i].similarity(vec);
+            System.out.print(ret[i]+" ");
+        }
+        System.out.println();
+        return ret;
+    }
+
+    public double[] maxWithIndex(double[] vec) {
+        double[] ret = { vec[0], 0 };
+        for (int i=1; i<vec.length; i++) {
+            if (vec[i]>ret[0]) {
+                ret[0] = vec[i];
+                ret[1] = i;
+            }
+        }
+        return ret;
     }
 
     public double[] code(double[] vec) {
-        double[] ret = new double[centroids.length];
-        for (int i=0; i<centroids.length; i++) {
-            ret[i] = centroids[i].similarity(vec);
-        }
+        double[] ret = new double[ncentr];
+        Arrays.fill(ret, 0d);
+
+        double[] mwi = maxWithIndex(similarities(vec));
+        System.out.println("Max: "+mwi[0]+" - idx: "+mwi[1]);
+        // here mwi[0] holds the max, if you need it
+        ret[(int)mwi[1]] = 1;
         return ret;
     }
 
-    // Wrote half asleep, refactor it
     public double[] reconstruct(double[] code) {
-        if (code.length != centroids.length) {
-            throw new RuntimeException(
-                "Code length does not match number of centroids.");
-        }
-        int imgsize = centroids[0].size;
+        checkLengths(code, ncentr);
         double[] ret = new double[imgsize];
-        for (int i=0; i<ret.length; i++) { ret[i] = 0d; }
-        for (int coord=0; coord<imgsize; coord++) {
-            for (int centr=0; centr<centroids.length; centr++) {
-                ret[coord] += code[centr]*(centroids[centr].getData()[coord]);
+        Arrays.fill(ret, 0d);
+        // for (int i=0; i<code.length; i++) {
+        //     System.out.print(code[i] + " ");
+        // }
+        // System.out.println();
+
+        for (int imgPos=0; imgPos<imgsize; imgPos++) {
+            for (int cIdx=0; cIdx<ncentr; cIdx++) {
+                ret[imgPos] += code[cIdx]*(centroids[cIdx].getData()[imgPos]);
             }
-            // ret[coord] /= centroids.length;
         }
-        return (new BMPLoader("","")).rescale(ret);
+        return ret; //(new BMPLoader("","")).rescale(ret);
     }
 
     public double reconstructionError(double[] orig, double[] reconstruction) {
-        if (orig.length != reconstruction.length) {
-            throw new RuntimeException(
-                "Original length does not match reconstruction length.");
-        }
+        checkLengths(orig, reconstruction);
         double ret = 0;
         // square error
         for (int i=0; i<orig.length; i++) {
-            ret += Math.exp(orig[i]-reconstruction[i], 2);
-        }
-        return ret;
-    }
-
-    public int maxidx(double[] vec) {
-        int ret=0;
-        double max=vec[ret];
-        for (int i=1; i<vec.length; i++) {
-            if (vec[i] > max) {
-                max = vec[i];
-                ret = i;
-            }
+            ret += Math.pow(orig[i]-reconstruction[i], 2);
         }
         return ret;
     }
 
     // train on single image
     public void train(double[] img) {
-        double[] code = code(img);
-        int closest = maxidx(code);
+        double[] closestWI = maxWithIndex(similarities(img));
+        // here you get the max in closestWI[0] if you need it
+        int closestIdx = (int)closestWI[1];
         // train the closest centroid
-        centroids[closest].train(img);
+        centroids[closestIdx].train(img);
     }
 
     // train on set of images
@@ -84,16 +110,19 @@ public class SVQ {
     }
 
     public double[][] getData() {
-        double[][] ret = new double[centroids.length][];
-        for (int i=0; i<centroids.length; i++) {
+        double[][] ret = new double[ncentr][];
+        for (int i=0; i<ncentr; i++) {
             ret[i] = centroids[i].getData();
         }
         return ret;
     }
 
     public static void main(String[] args) {
-        BMPLoader bmp = new BMPLoader("jaffe", "out");
 
+        // directory where the input images are located
+        String indir = "jaffe";
+        // directory where to place the output (centroids, etc)
+        String outdir = "out";
         // number of centroids
         int NCENTR  = 8;
         // size of training set
@@ -102,6 +131,7 @@ public class SVQ {
         int NVALID  = 4;
 
         // load images
+        BMPLoader bmp = new BMPLoader(indir, outdir);
         double[][] images = bmp.readAll(NTRAIN);
         System.out.println("Elaborating images: " +
             images.length + "x" + bmp.height + "x" + bmp.width);
@@ -115,6 +145,7 @@ public class SVQ {
         double[][] reconstructions = new double[NVALID][];
         double[] img, code;
         for (int i=0; i<NVALID; i++) {
+            // ten pseudorandom images, from training range
             img = images[10*i];
             bmp.save(img, "image_"+i);
             code = svq.code(img);
