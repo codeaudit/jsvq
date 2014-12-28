b069b663d0f8be6c653a461bd03c6d917ec042ca
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2014-12-28 20:12:50 +0100

    Added script to delete images from short runs
    It's still a first sketch.
    - Make it ask for confirmation of main directory before running
    - Take minimum dir size from input
    - Change into keeping the `n` largest dirs

---
+++ delete_bad.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mainfolder="${HOME}/torcs_imgs/"
+minfiles=5
+
+
+echo "Deleting all folders in '${mainfolder}' with less than ${minfiles} files."
+echo
+
+ls -d ${mainfolder}*/ |
+while read dir; do
+  echo "$(basename ${dir}/)"
+  nfiles=$(ls $dir | wc -w)
+  echo -en "\t${nfiles} files: "
+  if [[ $nfiles -lt $minfiles ]]; then
+    echo "DELETE"
+    rm -rf $dir
+  else
+    echo "KEEP"
+  fi
+done
