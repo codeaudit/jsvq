5fddb6beb3a630871053ae9ca16eea69c882ffe8
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2015-01-01 19:03:12 +0100

    Extracted SortableVec to its own class file

--- src/TrainingSet.java
+++ src/TrainingSet.java
@@ -13,33 +13,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
-// Java Generics gives problems to instantiate generic type arrays
-// as in `T[][] ret = new T[list.size()][];`
-// Since that's outside the scope of my work, I'll stick to hardcoded `int[]`s.
-class SortableVec implements Comparable {
-    public int[] vec;
-    // I could cache all similarities here
-    public double sim;
-
-    public SortableVec(int[] vec, double sim) {
-        this.vec = vec;
-        this.sim = sim;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        SortableVec rhs = (SortableVec)o;
-        // inverted for descending order
-        if (sim < rhs.sim) {
-            return 1;
-        } else if (sim > rhs.sim) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-}
-
 
 // Usage: an individual codes images with SVQ, which holds a unique TrainingSet.
 // Each time an image gets coded, it also tries to add it to the TS. Only the
